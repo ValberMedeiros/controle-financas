8e24b0b406141f4e801d10d19d2bac8c1f4a7c53
ValberMedeiros <dev.junior.medeiros@gmail.com>
2022-02-25 22:49:06 -0300

    Initial commit

---
+++ src/main/java/br/com/dev/valber/medeiros/controleficancas/ControleFicancasApplication.java
@@ -0,0 +1,13 @@
+package br.com.dev.valber.medeiros.controleficancas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleFicancasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControleFicancasApplication.class, args);
+    }
+
+}
